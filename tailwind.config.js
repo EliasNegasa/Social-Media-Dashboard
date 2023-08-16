031238a21d7c9708b680bd023351ce374ef22206
= <=>
2023-08-16 17:31:57 +0300

    darkmode classes added

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,14 @@ module.exports = {
         'bright-red': '#dc414c',
         'grayish-blue': {
           light: '#f0f2fa',
+          medium: '#e1e3f0',
           dark: '#63687e',
           'very-dark': '#1e202a',
+        },
+        'desaturated-blue': {
+          text: '#8b97c6',
+          medium: '#333a56',
+          dark: '#252a41',
         },
       },
       fontFamily: {
